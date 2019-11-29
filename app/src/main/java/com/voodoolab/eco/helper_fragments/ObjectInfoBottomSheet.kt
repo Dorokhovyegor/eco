09be7d692c17c1d8276e25621e002ffc7201c9d5
yegor <yegor17@gmail,com>
2019-11-29 20:23:18 +0300

    ботом щит диалог

---
+++ app/src/main/java/com/voodoolab/eco/helper_fragments/ObjectInfoBottomSheet.kt
@@ -0,0 +1,19 @@
+package com.voodoolab.eco.helper_fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.voodoolab.eco.R
+
+class ObjectInfoBottomSheet : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.object_info_layout, container,false)
+    }
+}
