b1417d8e3a0fbbc119cd3310f955b74c9d10f009
yegor <yegor17@gmail,com>
2019-12-05 18:50:48 +0300

    empty

--- app/src/main/java/com/voodoolab/eco/ui/tab_fragments/MapFragment.kt
+++ app/src/main/java/com/voodoolab/eco/ui/tab_fragments/MapFragment.kt
@@ -1,24 +1,48 @@
 package com.voodoolab.eco.ui.tab_fragments
 
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewStub
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.model.*
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
 import com.voodoolab.eco.R
 import com.voodoolab.eco.helper_fragments.ObjectInfoBottomSheet
 import com.voodoolab.eco.ui.MainActivity
 
+
 class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+    // todo https://github.com/googlemaps/android-samples/blob/master/ApiDemos/java/app/src/main/java/com/example/mapdemo/MyLocationDemoActivity.java
+    // todo https://developers.google.com/android/reference/com/google/android/gms/location/FusedLocationProviderApi
+    // todo https://developers.google.com/maps/documentation/android-sdk/location
 
     private var map: GoogleMap? = null
     private var mapView: MapView? = null
     private val MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey"
+
+    private var locationPermissionGranted = false
+    val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 145
+    var lastKnownLocation: LatLng? = null
+    val defaultLocation = LatLng(0.0, 0.0)
+    val DEFAULT_ZOOM = 15f
+
+    var geoDataClient = null
+    var placeDetectionClient = null
+    var fusedLocationProviderClient = null
 
     private val ecoVolgograd1 = LatLng(48.6809142, 44.4448137)
     private val ecoVolgograd2 = LatLng(48.5848916, 44.4207241)
@@ -34,12 +58,86 @@ class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListe
     private var pobedi: Marker? = null
     private var lenina: Marker? = null
 
+    private fun getLocationPermission() {
+
+        context?.let {
+            if (ContextCompat.checkSelfPermission(
+                    it.applicationContext,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                locationPermissionGranted = true
+            } else {
+
+                activity?.let { mainActivity ->
+                    if (mainActivity is MainActivity) {
+                        ActivityCompat.requestPermissions(mainActivity, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun updateLocationUI() {
+        map?.let {googleMap ->
+            if (locationPermissionGranted) {
+                map?.isMyLocationEnabled = true
+                map?.uiSettings?.isMyLocationButtonEnabled = true
+            } else {
+                map?.isMyLocationEnabled = false
+                map?.uiSettings?.isMyLocationButtonEnabled = false
+                lastKnownLocation = null
+                getLocationPermission()
+            }
+
+        }
+    }
+
+    private fun getDeviceLocation() {
+        try {
+            if (locationPermissionGranted) {
+                val locationResult: Task = mFusedLocationProviderClient.getLastLocation()
+                locationResult.addOnCompleteListener(this, object : OnCompleteListener {
+                    override fun onComplete(task: Task<*>) {
+                        if (task.isSuccessful) {
+                            // Set the map's camera position to the current location of the device.
+                            lastKnownLocation = task.result as LatLng
+                            lastKnownLocation?.let { location ->
+                                map?.moveCamera(
+                                    CameraUpdateFactory.newLatLngZoom(
+                                        LatLng(
+                                            location.latitude,
+                                            location.longitude
+                                        ), DEFAULT_ZOOM
+                                    )
+                                )
+                            }
+                        } else {
+                            map?.moveCamera(
+                                CameraUpdateFactory.newLatLngZoom(
+                                    defaultLocation,
+                                    DEFAULT_ZOOM
+                                )
+                            )
+                            map?.uiSettings?.isMyLocationButtonEnabled = false
+                        }
+                    }
+                })
+            }
+        } catch (e: SecurityException) {
+            println("DEBUG: ${e.message}")
+        }
+
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.map_fragment, container, false)
+
+        fusedLocationProviderClient = Location.
 
         return view
     }
@@ -68,39 +166,53 @@ class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListe
 
     override fun onMapReady(p0: GoogleMap?) {
         map = p0
+        updateLocationUI()
+        getDeviceLocation()
         map?.setOnMarkerClickListener(this)
-        val bitmap =  BitmapDescriptorFactory.fromResource(R.mipmap.selected_itemhdpi)
+        val bitmap = BitmapDescriptorFactory.fromResource(R.mipmap.selected_itemhdpi)
 
-        stadionnay = map?.addMarker(MarkerOptions()
-            .position(ecoVolgograd1)
-            .title("ул. Стадионная д. 20")
-            .icon(bitmap))
+        stadionnay = map?.addMarker(
+            MarkerOptions()
+                .position(ecoVolgograd1)
+                .title("ул. Стадионная д. 20")
+                .icon(bitmap)
+        )
 
         stadionnay?.tag = "sfa"
-        fruktovay = map?.addMarker(MarkerOptions()
-            .position(ecoVolgograd2)
-            .title("ул. Фруктовая, 9А")
-            .icon(bitmap))
+        fruktovay = map?.addMarker(
+            MarkerOptions()
+                .position(ecoVolgograd2)
+                .title("ул. Фруктовая, 9А")
+                .icon(bitmap)
+        )
 
-        heroes = map?.addMarker(MarkerOptions()
-            .position(ecoVolgograd3)
-            .title("пр-кт., Героев Сталинграда, 72А")
-            .icon(bitmap))
+        heroes = map?.addMarker(
+            MarkerOptions()
+                .position(ecoVolgograd3)
+                .title("пр-кт., Героев Сталинграда, 72А")
+                .icon(bitmap)
+        )
 
-        zhukova = map?.addMarker(MarkerOptions()
-            .position(ecoVolgograd4)
-            .title("пр-кт., им. Маршала Жукова, д. 55")
-            .icon(bitmap))
+        zhukova = map?.addMarker(
+            MarkerOptions()
+                .position(ecoVolgograd4)
+                .title("пр-кт., им. Маршала Жукова, д. 55")
+                .icon(bitmap)
+        )
 
-        pobedi = map?.addMarker(MarkerOptions()
-            .position(ecoVolgograd5)
-            .title("Бульвар 30 лет Победы, д. 1")
-            .icon(bitmap))
+        pobedi = map?.addMarker(
+            MarkerOptions()
+                .position(ecoVolgograd5)
+                .title("Бульвар 30 лет Победы, д. 1")
+                .icon(bitmap)
+        )
 
-        lenina = map?.addMarker(MarkerOptions()
-            .position(ecoVolgograd6)
-            .title("пр-кт., им. В.И. Ленина, 120Ж")
-            .icon(bitmap))
+        lenina = map?.addMarker(
+            MarkerOptions()
+                .position(ecoVolgograd6)
+                .title("пр-кт., им. В.И. Ленина, 120Ж")
+                .icon(bitmap)
+        )
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -141,5 +253,21 @@ class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListe
     override fun onLowMemory() {
         super.onLowMemory()
         mapView?.onLowMemory()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        locationPermissionGranted = false
+        when (requestCode) {
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    locationPermissionGranted = true
+                }
+            }
+        }
+        // todo update ui
     }
 }
